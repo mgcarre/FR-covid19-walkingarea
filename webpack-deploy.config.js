68f736f0fc3f273ef02799892a31cd08245509df
mgcarre <mathgb@gmail.com>
2020-03-28 23:37:14 +0100

    modif config webpack

---
+++ webpack-deploy.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: './dist/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new Dotenv({
+            safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+            systemvars: false, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+            silent: true, // hide any errors
+            defaults: false // load '.env.defaults' as the default values if empty.
+        })
+    ]
+};
